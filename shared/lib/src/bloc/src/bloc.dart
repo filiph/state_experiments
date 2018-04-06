8b3a11c1cbbd632d80aa749f6c8922210f30e367
Filip Hracek <filip.hracek@gmail.com>
2018-04-06 16:47:55 -0700

    Remove code of redundant BLoC implementations

--- shared/lib/src/bloc/src/bloc.dart
+++
@@ -1,9 +0,0 @@
-import 'package:reactive_exploration/src/bloc/disposer/disposer.dart';
-
-/// Implements the BLoC design pattern, which is a convention covered
-/// here:
-///
-/// * https://www.youtube.com/watch?v=PLHln7wHgPE
-///
-/// BLoC stands for Business Logic Layer Component.
-abstract class Bloc implements Disposable {}
