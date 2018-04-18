ba303721ab5c5f2ebe1ef256e1219736032a7669
Filip Hracek <filip.hracek@gmail.com>
2018-04-18 11:01:34 -0700

    Minimize congnitive load of MyHomePage
    Hardcode 'Flutter Demo Home Page' to the state instead of passing it down.

--- hello_world_start/lib/main.dart
+++ hello_world_start/lib/main.dart
@@ -10,16 +10,12 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
@@ -30,7 +26,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text(widget.title)),
+      appBar: new AppBar(title: new Text('Flutter Demo Home Page')),
       body: new Center(
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
