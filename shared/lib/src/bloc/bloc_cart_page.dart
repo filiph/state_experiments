839a2995b4073de9deae76d37148b641739e6428
Filip Hracek <filip.hracek@gmail.com>
2018-05-15 16:36:09 -0700

    Implement the CartPage in the same design as ScopedModel

--- shared/lib/src/bloc/bloc_cart_page.dart
+++ shared/lib/src/bloc/bloc_cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reactive_exploration/common/models/cart_item.dart';
+import 'package:reactive_exploration/common/widgets/cart_page.dart';
 import 'package:reactive_exploration/src/bloc/cart_provider.dart';
 
 class BlocCartPage extends StatelessWidget {
@@ -11,12 +12,22 @@ class BlocCartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final cart = CartProvider.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Your Cart"),
-      ),
-      body: StreamBuilder<List<CartItem>>(
-          stream: cart.items,
-          builder: (context, snapshot) => Text("Cart: ${snapshot.data}")),
-    );
+        appBar: AppBar(
+          title: Text("Your Cart"),
+        ),
+        body: StreamBuilder<List<CartItem>>(
+            stream: cart.items,
+            builder: (context, snapshot) {
+              if (snapshot.data == null) {
+                return Center(
+                    child: Text('Empty',
+                        style: Theme.of(context).textTheme.display1));
+              }
+
+              return ListView(
+                  children: snapshot.data
+                      .map((item) => ItemTile(item: item))
+                      .toList());
+            }));
   }
 }
