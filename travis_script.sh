bd68e2d12a0d89013da13c4c6df663ecdeb87472
Filip Hracek <filip.hracek@gmail.com>
2018-05-21 11:38:27 -0700

    Use matrix for testing different Flutter channels

--- travis_script.sh
+++ travis_script.sh
@@ -2,9 +2,6 @@ set -e
 
 cd ./shared
 
-# echo "== Testing on Flutter's stable channel =="
-# ../flutter/bin/flutter test
-# echo "-- Success --"
-echo "== Testing on Flutter's dev channel =="
-../flutter-dev/bin/flutter test
+echo "== Testing on Flutter's $FLUTTER_VERSION channel =="
+../flutter/bin/flutter test
 echo "-- Success --"
