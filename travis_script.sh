18405634d464de255c4a8381d1aa2d0706aa9083
Filip Hracek <filip.hracek@gmail.com>
2018-05-21 10:57:13 -0700

    Test on both stable and dev channels

--- travis_script.sh
+++ travis_script.sh
@@ -1,5 +1,9 @@
 set -e
 
 cd ./shared
-pwd
+echo "== Testing on Flutter's stable channel =="
 ../flutter/bin/flutter test
+echo "-- Success --"
+echo "== Testing on Flutter's dev channel =="
+../flutter-dev/bin/flutter test
+echo "-- Success --"
